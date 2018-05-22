42f894a330ab825752ff076f2f65e28b372b3eb3
Thilina Prasad <thilinaprasad.15@cse.mrt.ac.lk>
2018-05-22 11:31:37 +0530

    marker functionalities completed

--- main.js
+++ main.js
@@ -1,81 +1,94 @@
 const electron = require('electron'); //require electron here
 const url = require('url');  //electron permenet code segment
 const path = require('path');
-
-const {app,BrowserWindow,Menu,ipcMain} = electron;
+const childProcess = require("child_process");
+const { app, BrowserWindow, Menu, ipcMain } = electron;
 
 let mainMenu;
 
-app.on('ready',function(){
+//load old markers
+const storage = require('electron-json-storage');
+storage.setDataPath(__dirname + "/storage/markerData");
+let oldMarkers;
+
+storage.get('markerData', function (error, data) {
+    if (error) {
+        mainWindow.webContents.send('error',"Error Happned while saving data. Please try again."); 
+    }
+    oldMarkers = data;
+});
+//load old markers
+
+app.on('ready', function () {
     mainWindow = new BrowserWindow({});
     mainWindow.loadURL(url.format({
-        pathname:path.join(__dirname,'pages/mainWindow.html'),
-        protocol:'file',
-        slashes:true
+        pathname: path.join(__dirname, 'pages/mainWindow.html'),
+        protocol: 'file',
+        slashes: true
     }));
 
     //Quit app when closed main window
-mainWindow.on('closed',function(){
-    app.quit();
-});
+    mainWindow.on('closed', function () {
+        app.quit();
+    });
 
-//Bulild menu templete in window
-mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
-//Insert menu items
-Menu.setApplicationMenu(mainMenu);
+    //Bulild menu templete in window
+    mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+    //Insert menu items
+    Menu.setApplicationMenu(mainMenu);
 
 });
 
 // Menu Template
 const mainMenuTemplate = [
     {
-        label:'File',
-        submenu:[
+        label: 'File',
+        submenu: [
             {
-                label:'Add New Client',
+                label: 'Add New Client',
             },
             {
-                label:'View Clients',
+                label: 'View Clients',
             },
             {
-                label:'Quit',
+                label: 'Quit',
                 accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
-                click(){
+                click() {
                     app.quit();
                 }
             }
         ]
     },
     {
-        label:'View',
-        submenu:[
+        label: 'View',
+        submenu: [
             {
-                label:"Maximize",
-                accelerator:process.platform == 'darwin' ? 'Command+M' : 'Ctrl+M',
-                click(){
+                label: "Maximize",
+                accelerator: process.platform == 'darwin' ? 'Command+M' : 'Ctrl+M',
+                click() {
                     mainWindow.maximize();
                 }
             },
             {
-                label:'Minimize',
-                accelerator:process.platform == 'darwin' ? 'Command+W' : 'Ctrl+W',
-                click(){
+                label: 'Minimize',
+                accelerator: process.platform == 'darwin' ? 'Command+W' : 'Ctrl+W',
+                click() {
                     mainWindow.minimize();
                 }
             },
             {
-                label:'Present On',
-                accelerator:process.platform == 'darwin' ? 'Command+P' : 'Ctrl+P',
-                click(){
+                label: 'Present On',
+                accelerator: process.platform == 'darwin' ? 'Command+P' : 'Ctrl+P',
+                click() {
                     mainWindow.setFullScreen(true);
                     mainWindow.setMenu(null);
                     mainWindow.webContents.send('present:on');
                 }
             },
             {
-                label:'Present OFF',
-                accelerator:process.platform == 'darwin' ? 'Command+O' : 'Ctrl+O',
-                click(){
+                label: 'Present OFF',
+                accelerator: process.platform == 'darwin' ? 'Command+O' : 'Ctrl+O',
+                click() {
                     mainWindow.setFullScreen(false);
                     mainWindow.setMenu(mainMenu);
                     mainWindow.webContents.send('present:off');
@@ -90,40 +103,63 @@ const mainMenuTemplate = [
 
 
 //In Mac add empty object to file menu
-if(process.platform == 'darwin'){
+if (process.platform == 'darwin') {
     mainMenuTemplate.unshift({});
 }
 
 //Presentation Mode on
-ipcMain.on('present:on',function(e){
+ipcMain.on('present:on', function (e) {
     mainWindow.setFullScreen(true);
     mainWindow.setMenu(null);
 });
 
 //Present mode off 
-ipcMain.on('present:off',function(){
+ipcMain.on('present:off', function () {
     mainWindow.setFullScreen(false);
     mainWindow.setMenu(mainMenu);
 });
 
+//Open PPT
+ipcMain.on('open:ppt',function(e,file){
+    let file_path = path.join("storage/ppts",file);
+    childProcess.spawn(file_path, {shell: true});
+});
+
 //Dev tools for app
 //Add developer tools item for developer mode
-if(process.env.NODE_ENV !== 'production'){
+if (process.env.NODE_ENV !== 'production') {
     mainMenuTemplate.push(
         {
-            label:'Developer tools',
-            submenu:[
+            label: 'Developer tools',
+            submenu: [
                 {
-                    label:'Toggle Dev Tools',
-                    accelerator:process.platform == 'darwin' ? 'Command+I' : 'Ctrl+I',
-                    click(item,focusedWindow){
+                    label: 'Toggle Dev Tools',
+                    accelerator: process.platform == 'darwin' ? 'Command+I' : 'Ctrl+I',
+                    click(item, focusedWindow) {
                         focusedWindow.toggleDevTools();
                     }
                 },
                 {
-                    role:'reload'
+                    role: 'reload'
                 }
             ]
         }
     );
 }
+
+
+
+//save new marker data to json file
+ipcMain.on('save:marker', function (e, newmarker) {
+    console.log(oldMarkers);
+        storage.set('markerData', oldMarkers.concat(newmarker), function (error) {
+            if (error) { 
+                mainWindow.webContents.send('error',"Error Happned while saving data. Please try again."); 
+            } else {
+                mainWindow.webContents.send('success',"Successfully added new marker !."); 
+             }
+        });
+
+    
+});
+
--- scripts/app.js
+++ scripts/app.js
@@ -1,40 +1,41 @@
 // Internet Connection checker
-function checkNetConnection(){
-    var xhr = new XMLHttpRequest();
-    var file = "https://www.google.com/maps";
-    var r = Math.round(Math.random() * 10000);
-    xhr.open('HEAD', file + "?subins=" + r, false);
-    try {
-     xhr.send();
-     if (xhr.status >= 200 && xhr.status < 304) {
+function checkNetConnection() {
+  var xhr = new XMLHttpRequest();
+  var file = "https://www.google.com/maps";
+  var r = Math.round(Math.random() * 10000);
+  xhr.open('HEAD', file + "?subins=" + r, false);
+  try {
+    xhr.send();
+    if (xhr.status >= 200 && xhr.status < 304) {
       return true;
-     } else {
+    } else {
       return false;
-     }
-    } catch (e) {
-     return false;
     }
-   }
+  } catch (e) {
+    return false;
+  }
+}
 
-function connChecker(){
-    if(!checkNetConnection()){
-        $.confirm({
-            theme: 'supervan',
-            title: 'Connection Problem!',
-            content: 'Please connect to internet before running this app!',
-            columnClass: 'small',
-            buttons: {
-                tryAgain: {
-                    text: 'Try Again!',
-                    btnClass: 'btn-red',
-                    keys: ['enter'],
-                    action: function(){
-                        connChecker();
-                    }
-                }
-            }
-        });
-    }
+function connChecker() {
+  if (!checkNetConnection()) {
+    $.confirm({
+      theme: 'supervan',
+      title: 'Connection Problem!',
+      content: 'Please connect to internet before running this app!',
+      columnClass: 'small',
+      buttons: {
+        tryAgain: {
+          text: 'Try Again!',
+          btnClass: 'btn-red',
+          keys: ['enter'],
+          action: function () {
+            connChecker();
+            location.reload();
+          }
+        }
+      }
+    });
+  }
 }
 
 connChecker();
@@ -43,64 +44,117 @@ connChecker();
 //Initialize the map
 //Map Initialize
 
+const { ipcRenderer } = require('electron');
+
 var srilanka = { lat: 7.8731, lng: 80.7718 };
 var map = new google.maps.Map(document.getElementById('map'), {
-  zoom: 7.6,
+  zoom: 7.5,
   center: srilanka,
   mapTypeId: google.maps.MapTypeId.ROADMAP,
   fullscreenControl: true
 });
 infoWindow = new google.maps.InfoWindow(); // create info window object
 
-
 // image icon for marker
 var image = {
-    url: 'http://www.uomleos.org/wp-content/uploads/2018/04/marker_icon.png',
-  };
+  url: 'http://www.uomleos.org/wp-content/uploads/2018/04/marker_icon.png',
+};
+
+
+$.getJSON('../storage/markerData/markerData.json', function (data) {
 
   // Over lapping marker object
-  var oms = new OverlappingMarkerSpiderfier(map, {
-    markersWontMove: true,
-    markersWontHide: true,
-    basicFormatEvents: true
+var oms = new OverlappingMarkerSpiderfier(map, {
+  markersWontMove: true,
+  markersWontHide: true,
+  basicFormatEvents: true
+});
+
+  //Add MAS Marker
+  var coordinates = { lat: 6.8231014, lng: 79.8935539 };
+  var mas_marker = new google.maps.Marker({
+    position: coordinates,
+    file: 'MAS SLIDES',
+    icon: image,
+    animation: google.maps.Animation.BOUNCE,
+    map: map,
+    name: "MAS Intimates (Pvt) Ltd",
+    address: "7th Ln, Dehiwala-Mount Lavinia."
+
   });
 
-//Add Marker
-var coordinates = { lat: 6.8231014, lng: 79.8935539 };
-      var mas_marker = new google.maps.Marker({
+  oms.addMarker(mas_marker);
+  google.maps.event.addListener(mas_marker, 'dblclick', function () { // marker onclick event
+    // Presentation Open part goes here
+    //alert('Add Slide show to open : ' + mas_marker.file);
+  });
+
+  google.maps.event.addListener(mas_marker, 'spider_click', function () { // 
+
+  });
+
+  google.maps.event.addListener(mas_marker, 'click', function () {
+    var content = "<b>" + mas_marker.name + "</b><br>" + mas_marker.address + "<br><br>&diams;&nbsp;Click to spider all clients<br>&diams;&nbsp;Double tap to open presentation";
+    infoWindow.setContent(content);
+    infoWindow.open(map, mas_marker);
+  });
+
+  //marker hover effect
+  google.maps.event.addListener(mas_marker, 'mouseover', function () {
+    var content = "<b>" + mas_marker.name + "</b><br>" + mas_marker.address + "<br><br><i class='material-icons tiny'>beenhere</i>&nbsp;Click to spider all clients<br><i class='material-icons tiny'>beenhere</i>&nbsp;Double tap to open presentation";
+    infoWindow.setContent(content);
+    infoWindow.open(map, mas_marker);
+  });
+  google.maps.event.addListener(mas_marker, 'mouseout', function () {
+    infoWindow.close();
+  });
+
+  //ADD MAS MARKER
+
+  let new_id = data.length+1;
+
+  if (data.length > 0) {
+    for (var i = 0; i < data.length; i++) {
+      var place = data[i];
+      var coordinates = { lat: parseFloat(place.latitude), lng: parseFloat(place.longitude) };
+      var marker = new google.maps.Marker({
         position: coordinates,
-        file: 'MAS SLIDES',
+        file: place.file,
         icon: image,
-        animation: google.maps.Animation.BOUNCE,
+        animation: google.maps.Animation.DROP,
         map: map,
-        name: "MAS Intimates (Pvt) Ltd",
-        address:"7th Ln, Dehiwala-Mount Lavinia."
-
+        name: place.name,
+        address: place.address
       });
 
-      oms.addMarker(mas_marker); 
-      google.maps.event.addListener(mas_marker, 'dblclick', function () { // marker onclick event
+      oms.addMarker(marker);
+      google.maps.event.addListener(marker, 'dblclick', function () { // marker onclick event
         // Presentation Open part goes here
-        alert('Add Slide show to open : '+mas_marker.file);
+        console.log("dbl");
+        ipcRenderer.send('open:ppt',this.file);
       });
-      
-      google.maps.event.addListener(mas_marker, 'spider_click', function () { // 
-      
+
+      google.maps.event.addListener(marker, 'spider_click', function () { // 
+
       });
-    
-      google.maps.event.addListener(mas_marker, 'click', function () {
-        var content = "<b>"+mas_marker.name+"</b><br>"+mas_marker.address+"<br><br>&diams;&nbsp;Click to spider all clients<br>&diams;&nbsp;Double tap to open presentation";
+
+      google.maps.event.addListener(marker, 'click', function () {
+        var content = "<b>" + this.name + "</b><br>" + this.address + "<br><br>&diams;&nbsp;Click to spider all clients<br>&diams;&nbsp;Double tap to open presentation";
         infoWindow.setContent(content);
-        infoWindow.open(map, mas_marker);
+        infoWindow.open(map, this);
       });
-      
+
       //marker hover effect
-      google.maps.event.addListener(mas_marker, 'mouseover', function () {
-        var content = "<b>"+mas_marker.name+"</b><br>"+mas_marker.address+"<br><br><i class='material-icons tiny'>beenhere</i>&nbsp;Click to spider all clients<br><i class='material-icons tiny'>beenhere</i>&nbsp;Double tap to open presentation";
+      google.maps.event.addListener(marker, 'mouseover', function () {
+        var content = "<b>" + this.name + "</b><br>" + this.address + "<br><br><i class='material-icons tiny'>beenhere</i>&nbsp;Click to spider all clients<br><i class='material-icons tiny'>beenhere</i>&nbsp;Double tap to open presentation";
         infoWindow.setContent(content);
-        infoWindow.open(map, mas_marker);
+        infoWindow.open(map, this);
       });
-      google.maps.event.addListener(mas_marker, 'mouseout', function () {
+      google.maps.event.addListener(marker, 'mouseout', function () {
         infoWindow.close();
       });
 
+    }
+  }
+
+});
